b6c2262cdb40adaadf5f13bae66ba6efa573f349
Francesco <francesco@lescai.eu>
2021-12-14 23:21:21 +0100

    changed to correct library needed to install enrichplot and igraph dependency

--- amd64_v3.1.1/install.R
+++
@@ -1,5 +0,0 @@
-enrichurl <- "https://bioconductor.org/packages/release/bioc/src/contrib/enrichplot_1.14.1.tar.gz"
-install.packages(enrichurl, repos=NULL, type="source")
-
-clusterurl <- "https://bioconductor.org/packages/release/bioc/src/contrib/clusterProfiler_4.2.0.tar.gz"
-install.packages(clusterurl, repos=NULL, type="source")
